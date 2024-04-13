f23d9400c07c97c2aa437520f107d3455d3d0c54
ren-yamanashi <pinokionigg@icloud.com>
2024-04-13 16:58:38 +0900

    deploy ecs fargate

--- lib/ecs-fargate-cdk-stack.ts
+++ lib/ecs-fargate-cdk-stack.ts
@@ -1,16 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
-export class EcsFargateCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class EcsFargateCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsFargateCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const repository = ecr.Repository.fromRepositoryName(
+      this,
+      "SampleRepository",
+      "sample-node-app"  // 先ほど作ったリポジトリ名
+    );
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "FargateService",
+      {
+        cluster,
+        memoryLimitMiB: 512,
+        cpu: 256,
+        runtimePlatform: {
+          cpuArchitecture: ecs.CpuArchitecture.ARM64,
+        },
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromEcrRepository(repository),
+        },
+      }
+    );
   }
 }
